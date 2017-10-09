6f638f82263c9bf86b4d1001eefa3f795f473f1e
Josiah Furrer <jbfjosiah@gmail.com>
2017-10-09 04:09:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 
 var name = "Tom Petty"
 
+var holiday = "Halloween"
+
 
 function happyHolidays(string){
    return happyHolidays("Happy holidays!")
 }
 
 function happyHolidaysTo(string){
-  
+
 }
 
 function happyHolidayTo(string){
-  
+
 }
